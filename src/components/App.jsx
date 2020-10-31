ff5796e621227259e685d1ec2c8c00136337fa96
Christopher Martin <martincdev@gmail.com>
2020-10-31 17:28:12 -0500

    Added prettierrc.json and husky to ensure code quality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../scss/App.scss';
-import TitleGenerator from './TitleGenerator'
+import TitleGenerator from './TitleGenerator';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Great Turtle Title Generator</h3>
-        <TitleGenerator />
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h3>Great Turtle Title Generator</h3>
+				<TitleGenerator />
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/components/TitleGenerator.jsx
+++ src/components/TitleGenerator.jsx
@@ -1,60 +1,59 @@
 import React, { useState } from 'react';
-import TitleDisplay from './TitleDisplay'
-import TitleData from '../data/titles.json'
+import TitleDisplay from './TitleDisplay';
+import TitleData from '../data/titles.json';
 
 const TitleGenerator = () => {
-  const [titles, setTitles] = useState([]);
+	const [titles, setTitles] = useState([]);
 
-  const generateTitles = () => {
-    let randomArr = [];
-    let num;
+	const generateTitles = () => {
+		let randomArr = [];
+		let num;
 
-    while (randomArr.length < 3) {
-      num = Math.floor(Math.random() * Math.floor(9));
-      randomArr.push(num);
+		while (randomArr.length < 3) {
+			num = Math.floor(Math.random() * Math.floor(9));
+			randomArr.push(num);
 
-      randomArr = checkDuplicates(randomArr, num);
+			randomArr = checkDuplicates(randomArr, num);
+		}
+		retrieveTitles(randomArr);
+	};
 
-    }
-    retrieveTitles(randomArr);
-  }
+	const checkDuplicates = (arr, num) => {
+		if (arr.length >= 2) {
+			for (let i = 0; i < arr.length; i++) {
+				if (arr[i] === arr[i + 1]) {
+					arr.splice(i + 1, 1);
+				} else if (arr.length > 2 && arr[i] === arr[i + 2]) {
+					arr.splice(i + 2, 1);
+				}
+			}
+		}
 
-  const checkDuplicates = (arr, num) => {
+		return arr;
+	};
 
-    if (arr.length >= 2) {
-      for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === arr[i + 1]) {
-          arr.splice(i + 1, 1);
-        } else if (arr.length > 2 && arr[i] === arr[i + 2]) {
-          arr.splice(i + 2, 1);
-        }
-      }
-    }
+	const retrieveTitles = (arr) => {
+		let results = [];
+		arr.forEach((num) => {
+			results.push(TitleData.data[num]);
+		});
 
-    return arr;
-  }
+		setTitles(results);
+	};
 
-  const retrieveTitles = (arr) => {
-    let results = [];
-    arr.forEach(num => {
-      results.push(TitleData.data[num]);
-    })
-
-    setTitles(results);
-  }
-
-  console.log(titles)
-  return (
-    <>
-      <div className="title-inputs">
-        <button className="title-button" onClick={generateTitles}>Generate</button>
-      </div>
-      <TitleDisplay titles={titles} />
-    </>
-  );
-}
+	return (
+		<>
+			<div className='title-inputs'>
+				<button className='title-button' onClick={generateTitles}>
+					Generate
+				</button>
+			</div>
+			<TitleDisplay titles={titles} />
+		</>
+	);
+};
 
 // TODO: Allow user to input name for titles with name in it
-// <input type="text" />
+// <input type='text' />
 
 export default TitleGenerator;
